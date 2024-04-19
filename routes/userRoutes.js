534c1ef381bbf352da1263aa627de88a74604156
harshl989 <harshitgoyal0613@gmail.com>
2024-04-20 01:54:56 +0530

    edited some errors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.put('/update-details', userController.updateUserDetails);
+router.get('/', ()=>console.log('hello'));
 
 module.exports = router;
